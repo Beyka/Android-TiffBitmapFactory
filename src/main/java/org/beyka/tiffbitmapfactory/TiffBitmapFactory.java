10ad23034ce8b0c46d5c3935a22d178ebdc44521
Bey, Alexey <alexey.bey@oxagile.com>
2015-11-23 11:01:50 +0300

    Change build.gradle to remove dump dependencys. Fix javadoc.

--- src/main/java/org/beyka/tiffbitmapfactory/TiffBitmapFactory.java
+++ src/main/java/org/beyka/tiffbitmapfactory/TiffBitmapFactory.java
@@ -171,12 +171,12 @@ public class TiffBitmapFactory {
         public boolean inJustDecodeBounds;
 
         /**
-         * If set to a value > 1, requests the decoder to subsample the original
+         * If set to a value &gt; 1, requests the decoder to subsample the original
          * image, returning a smaller image to save memory. The sample size is
          * the number of pixels in either dimension that correspond to a single
          * pixel in the decoded bitmap. For example, inSampleSize == 4 returns
          * an image that is 1/4 the width/height of the original, and 1/16 the
-         * number of pixels. Any value <= 1 is treated the same as 1. Note: the
+         * number of pixels. Any value &lt;= 1 is treated the same as 1. Note: the
          * decoder uses a final value based on powers of 2, any other value will
          * be rounded down to the nearest power of 2.
          */
