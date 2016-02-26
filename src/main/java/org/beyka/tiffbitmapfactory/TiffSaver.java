cce454d35324a0b81e3ef36b12ad571a812624a1
Aleksey Bey <aleksey.bey@teqniksoft.com>
2016-02-26 09:23:57 +0300

    Remove unused code. Code formating

--- src/main/java/org/beyka/tiffbitmapfactory/TiffSaver.java
+++ src/main/java/org/beyka/tiffbitmapfactory/TiffSaver.java
@@ -22,65 +22,21 @@ public class TiffSaver {
         /**
          * No compression
          */
-        COMPRESSION_NONE (1),
-//
-//        /**
-//         * CCITT modified Huffman RLE
-//         */
-//        COMPRESSION_CCITTRLE (2),
-//
-//        /**
-//         * CCITT Group 3 fax encoding
-//         */
-//        COMPRESSION_CCITTFAX3 (3),
-//
-//        /**
-//         * CCITT Group 4 fax encoding
-//         */
-//        COMPRESSION_CCITTFAX4 (4),
-//
+        COMPRESSION_NONE(1),
         /**
          * LZW
          */
-        COMPRESSION_LZW (5),
-//
-//        /**
-//         * JPEG ('old-style' JPEG, later overriden in Technote2)
-//         */
-//        COMPRESSION_OJPEG (6),
-//
+        COMPRESSION_LZW(5),
         /**
          * JPEG ('new-style' JPEG)
          */
-        COMPRESSION_JPEG (7),
-//
-//        COMPRESSION_NEXT (32766),
-//        COMPRESSION_CCITTRLEW (32771),
-//
-//        /**
-//         * PackBits compression, aka Macintosh RLE
-//         */
-        COMPRESSION_PACKBITS (32773),
-//        COMPRESSION_THUNDERSCAN (32809),
-//        COMPRESSION_IT8CTPAD (32895),
-//        COMPRESSION_IT8LW (32896),
-//        COMPRESSION_IT8MP (32897),
-//        COMPRESSION_IT8BL (32898),
-//        COMPRESSION_PIXARFILM (32908),
-//        COMPRESSION_PIXARLOG (32909),
-        COMPRESSION_DEFLATE (32946),
-//
-//        /**
-//         * Deflate ('Adobe-style')
-//         */
-        COMPRESSION_ADOBE_DEFLATE (8);
-//        COMPRESSION_DCS (32947),
-//        COMPRESSION_JBIG (34661),
-//        COMPRESSION_SGILOG (34676),
-//        COMPRESSION_SGILOG24 (34677),
-//        COMPRESSION_JP2000 (34712);
+        COMPRESSION_JPEG(7),
+        COMPRESSION_PACKBITS(32773),
+        COMPRESSION_DEFLATE(32946),
+        COMPRESSION_ADOBE_DEFLATE(8);
 
         final int ordinal;
+
         CompressionMode(int ordinal) {
             this.ordinal = ordinal;
         }
@@ -97,6 +53,7 @@ public class TiffSaver {
         ORIENTATION_LEFTBOT(8);
 
         final int ordinal;
+
         Orientation(int ordinal) {
             this.ordinal = ordinal;
         }
@@ -104,10 +61,10 @@ public class TiffSaver {
 
     /**
      * Save bitmap to file with default {@link TiffSaver.SaveOptions options}.
-     * @param destination - file to write bitmap
-     * @param bmp - Bitmap for saving
-     * @return true if bitmap was saved successful or false otherwise
      *
+     * @param destination - file to write bitmap
+     * @param bmp         - Bitmap for saving
+     * @return true if bitmap was saved successful or false otherwise
      * @throws org.beyka.tiffbitmapfactory.exceptions.NoSuchFileException when {@code destination} not exist or can't be opened for writing
      */
     public static boolean saveBitmap(File destination, Bitmap bmp) throws NoSuchFileException {
@@ -116,11 +73,11 @@ public class TiffSaver {
 
     /**
      * Save bitmap to file.
-     * @param destination - file to write bitmap
-     * @param bmp - Bitmap for saving
-     * @param options - options for saving
-     * @return true if bitmap was saved successful or false otherwise
      *
+     * @param destination - file to write bitmap
+     * @param bmp         - Bitmap for saving
+     * @param options     - options for saving
+     * @return true if bitmap was saved successful or false otherwise
      * @throws org.beyka.tiffbitmapfactory.exceptions.NoSuchFileException when {@code destinationPath} not exist or can't be opened for writing
      */
     public static boolean saveBitmap(File destination, Bitmap bmp, SaveOptions options) throws NoSuchFileException {
@@ -129,10 +86,10 @@ public class TiffSaver {
 
     /**
      * Save bitmap to file with default {@link TiffSaver.SaveOptions options}.
-     * @param destinationPath - file path to write bitmap
-     * @param bmp - Bitmap for saving
-     * @return true if bitmap was saved successful or false otherwise
      *
+     * @param destinationPath - file path to write bitmap
+     * @param bmp             - Bitmap for saving
+     * @return true if bitmap was saved successful or false otherwise
      * @throws org.beyka.tiffbitmapfactory.exceptions.NoSuchFileException when {@code destinationPath} not exist or can't be opened for writing
      */
     public static boolean saveBitmap(String destinationPath, Bitmap bmp) throws NoSuchFileException {
@@ -141,11 +98,11 @@ public class TiffSaver {
 
     /**
      * Save bitmap to file.
-     * @param destinationPath - file path to write bitmap
-     * @param bmp - Bitmap for saving
-     * @param options - options for saving
-     * @return true if bitmap was saved successful or false otherwise
      *
+     * @param destinationPath - file path to write bitmap
+     * @param bmp             - Bitmap for saving
+     * @param options         - options for saving
+     * @return true if bitmap was saved successful or false otherwise
      * @throws org.beyka.tiffbitmapfactory.exceptions.NoSuchFileException when {@code destinationPath} not exist or can't be opened for writing
      */
     public static boolean saveBitmap(String destinationPath, Bitmap bmp, SaveOptions options) throws NoSuchFileException {
@@ -169,13 +126,13 @@ public class TiffSaver {
 
         /**
          * Compression scheme used on the image data.
-         * <p>Default value is {@link CompressionMode#COMPRESSION_NONE}</p>
+         * <p>Default value is {@link CompressionMode#COMPRESSION_NONE COMPRESSION_NONE}</p>
          */
         public CompressionMode compressionMode;
 
         /**
          * {@link org.beyka.tiffbitmapfactory.TiffSaver.Orientation Orientation} that will used for saving image
-         * <p>By default uses {@link org.beyka.tiffbitmapfactory.TiffSaver.Orientation#ORIENTATION_TOPLEFT} </p>
+         * <p>By default uses {@link org.beyka.tiffbitmapfactory.TiffSaver.Orientation#ORIENTATION_TOPLEFT ORIENTATION_TOPLEFT} </p>
          */
         public Orientation orientation;
 
