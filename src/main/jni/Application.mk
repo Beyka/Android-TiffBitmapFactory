9d12a4c6a05bb28e75b3dbcf3f32e404bbd6297b
Beyka <leshabey@gmail.com>
2016-11-26 09:57:39 +0000

    Added support  for all abis.

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,4 +4,6 @@ APP_MODULES      := tiff tifffactory tiffsaver
 
 APP_PLATFORM=android-8
 
-APP_ABI := x86 armeabi armeabi-v7a mips
+APP_ABI := all
+#armeabi-v7a arm64-v8a x86 x86_64 mips mips64
+#x86 armeabi armeabi-v7a mips
