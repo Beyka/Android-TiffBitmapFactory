429cd82cce96e9ced291c2032be3d513366dc49a
Aleksey Bey <aleksey.bey@teqniksoft.com>
2016-12-22 09:59:58 +0300

    Hotfix

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -6,4 +6,4 @@ APP_PLATFORM=android-8
 
 #Do not build x32 and x64 modules in same time - it leads to errors in binaries
 APP_ABI := armeabi-v7a x86 armeabi mips
-#arm64-v8a x86_64 mips64
+#APP_ABI := arm64-v8a x86_64 mips64
