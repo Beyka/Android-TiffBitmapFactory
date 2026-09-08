98efd9d55d3394599da90a0386f38cb4ed73d9d9
Aleksey Bey <beyka.dev@gmail.com>
2026-09-08 08:54:43 +0300

    ABI

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,7 +4,7 @@ APP_MODULES      := tiff png jpeg tifffactory tiffsaver tiffconverter
 
 APP_PLATFORM=android-21
 
-APP_ABI := arm64-v8a, armeabi-v7a
+APP_ABI := all
 
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 APP_LDFLAGS += -Wl,-z,max-page-size=16384 -Wl,-z,common-page-size=16384
